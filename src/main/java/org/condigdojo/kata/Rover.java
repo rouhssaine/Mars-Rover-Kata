ad1010daa202c048f3257aa75db3fc7a9004a230
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2023-03-05 12:25:28 +0100

    [MarsRover] GREEN : commands RRR

--- src/main/java/org/condigdojo/kata/Rover.java
+++ src/main/java/org/condigdojo/kata/Rover.java
@@ -6,6 +6,7 @@ public class Rover {
 
     public String execute(String commands) {
         commands.chars().forEach(c -> {
+            if (direction == 'S') direction = 'W';
             if (direction == 'E') direction = 'S';
             if (direction == 'N') direction = 'E';
         });
